8de41a4e962c428caff23ecc3afe1835519d640c
Lukas Arnold <l.arnold@fz-juelich.de>
2017-08-17 15:58:21 +0200

    added comments for cone example input file

--- examples/cone_01/input.py
+++ examples/cone_01/input.py
@@ -2,40 +2,35 @@
 # define variable 'setups': simulation setup set
 # define variable 'optimiser': properties for the optimiser
 
+# import just for IDE convenience
 import propti as pr
 
+# fix the chid
 CHID = 'CONE'
 
+# define the optimisation parameter
 op1 = pr.Parameter(name='thickness1', place_holder='thickness1',
                    min_value=1e-3, max_value=1e-1)
-
 op2 = pr.Parameter(name='thickness2', place_holder='thickness2',
                    min_value=1e-3, max_value=1e-1)
+ops = pr.ParameterSet(params=[op1, op2])
 
-ops = pr.ParameterSet()
-ops.append(op1)
-ops.append(op2)
+# define general model parameter, including optimisation parameter
+mps0 = pr.ParameterSet(params=[op1, op2])
+mps0.append(pr.Parameter(name='heat flux', place_holder='exflux', value=75))
+mps0.append(pr.Parameter(name='tend', place_holder='tend'))
+mps0.append(pr.Parameter(name='mesh_i', place_holder='i', value=3))
+mps0.append(pr.Parameter(name='mesh_j', place_holder='j', value=3))
+mps0.append(pr.Parameter(name='mesh_k', place_holder='k', value=4))
+mps0.append(pr.Parameter(name='chid', place_holder='filename', value=CHID))
 
-mp1 = pr.Parameter(name='heat flux', place_holder='exflux', value=75)
-mp2 = pr.Parameter(name='tend', place_holder='tend')
-mp3 = pr.Parameter(name='mesh_i', place_holder='i', value=3)
-mp4 = pr.Parameter(name='mesh_j', place_holder='j', value=3)
-mp5 = pr.Parameter(name='mesh_k', place_holder='k', value=4)
-mp6 = pr.Parameter(name='chid', place_holder='filename', value=CHID)
-
-mps0 = pr.ParameterSet()
-mps0.append(op1)
-mps0.append(op2)
-mps0.append(mp1)
-mps0.append(mp2)
-mps0.append(mp3)
-mps0.append(mp4)
-mps0.append(mp5)
-mps0.append(mp6)
-
+# define empty simulation setup set
 setups = pr.SimulationSetupSet()
+
+# loop over all 'iso' values
 for iso in ['Alu', 'ISO']:
 
+    # define model-experiment data relation
     r = pr.Relation()
     r.model.file_name = "{}_hrr.csv".format(CHID)
     r.model.label_x = 'Time'
@@ -47,15 +42,17 @@ for iso in ['Alu', 'ISO']:
     r.experiment.header_line = 0
     r.experiment.factor = 1e-3
 
+    # use above model prototype (mps0) as template
     mps = copy.deepcopy(mps0)
 
-    # r.read_data(wd='.', target='experiment')
-    TEND = 600 # r.experiment.x[-1]
-    # print('tend: {}'.format(TEND))
+    TEND = 600
+    # modify a single value of model parameter
     mps[3].value = TEND
 
+    # define definition set for data comparison
     r.x_def = np.arange(0., TEND, 1)
 
+    # create simulation setup object
     template_file = "SimpleConeLaunchTest_{}_BestParaSet_Toast.fds".format(iso)
     s = pr.SimulationSetup(name='cone_{}'.format(iso),
                            work_dir='cone_{}'.format(iso),
@@ -64,6 +61,8 @@ for iso in ['Alu', 'ISO']:
                            model_executable='fds',
                            relationship_model_experiment=[r])
 
+    # append above object to simulation setup set
     setups.append(s)
 
+# use default values for optimiser
 optimiser = pr.OptimiserProperties()
