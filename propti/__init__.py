d9ef8d46c514e06578d635a5c9847709d6a72d70
Lukas Arnold <l.arnold@fz-juelich.de>
2017-08-18 13:55:59 +0200

    formatting/ comments

--- propti/__init__.py
+++ propti/__init__.py
@@ -5,21 +5,30 @@ from .data_structures import Parameter, ParameterSet, \
 
 import logging
 
+#########
+# LOGGING
 # set up logging to file - see previous section for more details
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
                     filename='propti.log',
                     filemode='w')
+
 # define a Handler which writes INFO messages or higher to the sys.stderr
 console = logging.StreamHandler()
 console.setLevel(logging.INFO)
+
 # set a format which is simpler for console use
 formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+
 # tell the handler to use this format
 console.setFormatter(formatter)
+
 # add the handler to the root logger
 logging.getLogger('').addHandler(console)
 
+###########
+# CONSTANTS
 
+# TODO: respect this variable in scripts
 pickle_prefix = 'propti.pickle'
