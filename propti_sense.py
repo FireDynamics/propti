c46aa74882b1e92cb5d8294273a131ff275e7d27
Ashish Vinayak <ashish.vinayak@gmail.com>
2017-12-15 12:03:33 +0100

    Added more functionality. The OptimiserProperties for FAST are selected based on the OptimiserProperties for optimisation algorithm

--- propti_sense.py
+++ propti_sense.py
@@ -18,23 +18,50 @@ ops = None  # type: pr.ParameterSet
 optimiser = None  # type: pr.OptimiserProperties
 
 in_file = open('propti.pickle.init', 'rb')
-setups, ops, *rest = pickle.load(in_file)
+setups, ops, optimiser = pickle.load(in_file)
 in_file.close()
-# set optimiser to perform FAST sensitivity analysis.
-# This will not overwrite pickle file, but create a new one.
-k = len(ops)        # total number of optimization params
-(M, d) = 3, 2       # M = inference factor, d = freq. step <spotpy defaults>
-rep = (1 + 4*(1 + (k-2)*d)*M**2)*k
-optimiser = pr.OptimiserProperties('fast', repetitions=rep, mpi=False)
-
+# Check if everything is in order
 if ops is None:
     logging.critical("optimisation parameter are not defined")
 if setups is None:
     logging.critical("simulation setups are not defined")
+if optimiser is None:
+    logging.critical("optimiser properties are not defined")
 
-print(setups, ops, optimiser)
+# Optimiser to perform FAST sensitivity analysis.
+
+# Use the same data from optimiser for sensitivity analysis
+num_subproc = optimiser.num_subprocesses
+mpi_bool = optimiser.mpi
+backup = optimiser.backup_every
+
+# Compute number of repetitions required
+k = len(ops)        # total number of optimization params
+(M, d) = 3, 2       # M = inference factor, d = freq. step <spotpy defaults>
+rep = (1 + 4*(1 + (k-2)*d)*M**2)*k
+
+
+sensitivity = pr.OptimiserProperties('fast',
+                                     repetitions=rep,
+                                     backup_every=backup,
+                                     db_name="propti_sensitivity_db",
+                                     db_type="csv",
+                                     num_subprocesses=num_subproc,
+                                     mpi=mpi_bool)
+
+
+print(setups, ops, sensitivity)
+'''
+Not writing any data to pickle file since there is only
+one method to analyse sensitivity. It makes sense to Wiki this instead.
+'''
+# Write sensitivity pickle file
+# out_file = open('propti_sensitivity.pickle.init', 'wb')
+# pickle.dump((setups, ops, optimiser), out_file)
+# out_file.close()
+
 # Run optimization.
 # Output of run_optimisation will be None.
 # What are some good parameters that can be printed ?
-res = pr.run_optimisation(ops, setups, optimiser)
+# res = pr.run_optimisation(ops, setups, sensitivity)
 # print(res) # output of res is None.
