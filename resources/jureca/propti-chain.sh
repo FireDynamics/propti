43e6828ea49bfdc998ac22eece8bc3a5e6c65d8d
Tristan Hehnen <tristan.hehnen@cern.ch>
2018-07-19 12:54:15 +0000

    Update propti-chain.sh. Small correction. Added code to proceed with the job, after it has crashed.

--- resources/jureca/propti-chain.sh
+++ resources/jureca/propti-chain.sh
@@ -5,15 +5,25 @@
 NO_OF_JOBS=XX
 
 # define jobscript
-JOB_SCRIPT=fds-chain-part-modified.job
+JOB_SCRIPT=propti-chain-part.job
 
 echo "sbatch ${JOB_SCRIPT}"
 JOBID=$(sbatch ${JOB_SCRIPT} 2>&1 | awk '{print $(NF)}')
 
-# Launch the next job, after the previous one has been finished successfully.
+## Launch the next job, after the previous one has been finished successfully.
+#I=0
+#while [ ${I} -le ${NO_OF_JOBS} ]; do
+#  echo "sbatch -d afterok:${JOBID} ${JOB_SCRIPT}"
+#  JOBID=$(sbatch -d afterok:${JOBID} ${JOB_SCRIPT} 2>&1 | awk '{print $(NF)}')
+#  let I=${I}+1
+#done
+
+
+
+# Launch the next job, after the previous one has been terminated in some failed state (non-zero exit code, node failure, timed out, etc.).
 I=0
 while [ ${I} -le ${NO_OF_JOBS} ]; do
-  echo "sbatch -d afterok:${JOBID} ${JOB_SCRIPT}"
-  JOBID=$(sbatch -d afterok:${JOBID} ${JOB_SCRIPT} 2>&1 | awk '{print $(NF)}')
+  echo "sbatch -d afternotok:${JOBID} ${JOB_SCRIPT}"
+  JOBID=$(sbatch -d afternotok:${JOBID} ${JOB_SCRIPT} 2>&1 | awk '{print $(NF)}')
   let I=${I}+1
 done
