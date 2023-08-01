7e2646d756dcb885822a35f73eae2daba2136c80
Xanthron Writer <xanthronwriter@gmail.com>
2023-08-01 11:09:09 +0200

    Separated run from library loading. Moved main() in __init__ to __main__ so the whole library does not have to be loaded. This improves performances.

--- propti/__init__.py
+++ propti/__init__.py
@@ -1,31 +1,4 @@
+__version__ = "1.0.10"
+# __version__ must be the fist line in order for setup.py to read the version.
+
 from .lib import *
-
-__version__ = "1.0.0"
-
-def main():
-    import argparse
-    import sys
-
-    commands = ["analyse","prepare","run","sampler","sense"]
-
-    command =  sys.argv[1] if len(sys.argv) > 1 else None
-    if command in commands:
-        sys.argv.pop(1)
-        if command == "analyse":
-            from .run import propti_analyse
-        elif command == "prepare":
-            from .run import propti_prepare
-        elif command == "run":
-            from .run import propti_run
-        elif command == "sampler":
-            from .run import propti_sampler
-        elif command == "sense":
-            from .run import propti_sense
-    else:
-        parser = argparse.ArgumentParser(
-                            prog='propti',
-                            description='Test calling different sub programs',
-                            epilog="use: 'propti <command> -h' for more information about the sub program.")
-        parser.add_argument("command",choices=commands)
-        parser.add_argument("args", nargs="*")
-        parser.parse_args()
